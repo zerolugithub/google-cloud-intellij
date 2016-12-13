3221b311ac772dcf692e6621636a779f5df8db71
Alex Sloan <alexsloan@google.com>
2016-12-13 18:22:14 -0500

    Refactoring of AppEngineApplicationCreateDialog

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/application/AppEngineApplicationCreateDialog.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/application/AppEngineApplicationCreateDialog.java
@@ -26,44 +26,41 @@ import com.google.cloud.tools.intellij.util.GctBundle;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.JBColor;
-import com.intellij.util.ui.JBFont;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.Component;
-import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
-import javax.swing.ListCellRenderer;
 
 public class AppEngineApplicationCreateDialog extends DialogWrapper {
 
-  private final static String STANDARD_ENV_AVAILABLE_KEY = "standardEnvironmentAvailable";
-  private final static String FLEXIBLE_ENV_AVAILABLE_KEY = "flexibleEnvironmentAvailable";
-  private final static String DOCUMENTATION_URL
+  private final static String LOCATIONS_DOCUMENTATION_URL
       = "https://cloud.google.com/docs/geography-and-regions";
+  private final static String FLEX_DOCUMENTATION_URL
+      = "https://cloud.google.com/appengine/docs/flexible/";
+  private final static String STANDARD_DOCUMENTATION_URL
+      = "https://cloud.google.com/appengine/docs/about-the-standard-environment";
   private static final String HTML_OPEN_TAG = "<html><font face='sans' size='-1'>";
   private static final String HTML_CLOSE_TAG = "</font></html>";
 
   private JPanel panel;
   private JTextPane instructionsTextPane;
-  private JComboBox<Location> regionComboBox;
+  private JComboBox<AppEngineLocationSelectorItem> regionComboBox;
   private JTextPane statusPane;
+  private JTextPane regionDetailPane;
 
   private Component parent;
   private Credential userCredential;
   private String gcpProjectId;
-  private List<ApplicationCreatedListener> applicationCreatedListeners;
 
   public AppEngineApplicationCreateDialog(@NotNull Component parent, @NotNull String gcpProjectId,
       @NotNull Credential userCredential) {
@@ -72,27 +69,34 @@ public class AppEngineApplicationCreateDialog extends DialogWrapper {
     this.parent = parent;
     this.gcpProjectId = gcpProjectId;
     this.userCredential = userCredential;
-    this.applicationCreatedListeners = new ArrayList<>();
 
     init();
     setTitle(GctBundle.message("appengine.application.region.select"));
     refreshLocationsSelector();
 
-    regionComboBox.setRenderer(new AppEngineRegionComboBoxRenderer());
+    regionComboBox.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        if (e.getStateChange() == ItemEvent.SELECTED) {
+          updateLocationDetailMessage();
+        }
+      }
+    });
 
+    regionDetailPane.addHyperlinkListener(new BrowserOpeningHyperLinkListener());
     instructionsTextPane.addHyperlinkListener(new BrowserOpeningHyperLinkListener());
     instructionsTextPane.setText(HTML_OPEN_TAG
         + GctBundle.message("appengine.application.create.instructions")
         + "<p>"
         + GctBundle.message("appengine.application.create.documentation",
-        "<a href=\"" + DOCUMENTATION_URL + "\">", "</a>")
+        "<a href=\"" + LOCATIONS_DOCUMENTATION_URL + "\">", "</a>")
         + "</p>" + HTML_CLOSE_TAG);
   }
 
   @Override
   protected void doOKAction() {
-    Location selectedLocation = (Location) regionComboBox.getSelectedItem();
-    final String locationId = getLocationId(selectedLocation);
+    final Location selectedLocation
+        = ((AppEngineLocationSelectorItem) regionComboBox.getSelectedItem()).getLocation();
 
     // show loading state
     setOKActionEnabled(false);
@@ -105,7 +109,7 @@ public class AppEngineApplicationCreateDialog extends DialogWrapper {
         Application result;
         try {
           result = AppEngineAdminService.getInstance().createApplication(
-              locationId, gcpProjectId, userCredential);
+              selectedLocation.getLocationId(), gcpProjectId, userCredential);
 
         } catch (IOException e) {
           setStatusMessageAsync(GctBundle.message("appengine.application.create.error"), true);
@@ -118,10 +122,6 @@ public class AppEngineApplicationCreateDialog extends DialogWrapper {
           return;
         }
 
-        for (ApplicationCreatedListener listener : applicationCreatedListeners) {
-          listener.onApplicationCreated(result);
-        }
-
         // Defer to the dispatch thread to invoke the action of closing this dialog
         ApplicationManager.getApplication().invokeLater(new Runnable() {
           @Override
@@ -131,14 +131,6 @@ public class AppEngineApplicationCreateDialog extends DialogWrapper {
         });
       }
     });
-  }
-
-  public void addApplicationCreatedListener(ApplicationCreatedListener listener) {
-    applicationCreatedListeners.add(listener);
-  }
-
-  public void removeApplicationCreatedListener(ApplicationCreatedListener listener) {
-    applicationCreatedListeners.remove(listener);
   }
 
   private void setStatusMessageAsync(final String message, final boolean isError) {
@@ -156,77 +148,49 @@ public class AppEngineApplicationCreateDialog extends DialogWrapper {
     statusPane.setVisible(true);
   }
 
-  private String getLocationId(Location location) {
-    // TODO(alexsloan) when b/33458530 is addressed, we can just use location.getLocationId()
-    return location.getLabels().get("cloud.googleapis.com/region");
-
-  }
-
   private void refreshLocationsSelector() {
     List<Location> appEngineRegions;
     try {
-      appEngineRegions = AppEngineAdminService.getInstance().getAllAppEngineLocations(userCredential);
+      appEngineRegions = AppEngineAdminService.getInstance()
+          .getAllAppEngineLocations(userCredential);
     } catch (IOException e) {
       setStatusMessage(GctBundle.message("appengine.application.region.list.fetch.error"), true);
       return;
     }
+
     regionComboBox.removeAllItems();
     for (Location location : appEngineRegions) {
-      regionComboBox.addItem(location);
+      regionComboBox.addItem(new AppEngineLocationSelectorItem(location));
     }
+
+    updateLocationDetailMessage();
+  }
+
+  private void updateLocationDetailMessage() {
+    AppEngineLocationSelectorItem item
+        = (AppEngineLocationSelectorItem) regionComboBox.getSelectedItem();
+
+    String displayText = HTML_OPEN_TAG + "<ul>";
+
+    if (item.isStandardSupported()) {
+      displayText += "<li>" + GctBundle.message("appengine.application.region.supports.standard",
+          "<a href=\"" + STANDARD_DOCUMENTATION_URL + "\">", "</a>")
+          + "</li>";
+    }
+    if (item.isFlexSupported()) {
+      displayText += "<li>" + GctBundle.message("appengine.application.region.supports.flex",
+          "<a href=\"" + FLEX_DOCUMENTATION_URL + "\">", "</a>")
+          + "</li>";
+    }
+
+    displayText += "</ul>" + HTML_CLOSE_TAG;
+    regionDetailPane.setText(displayText);
   }
 
   @Nullable
   @Override
   protected JComponent createCenterPanel() {
     return panel;
-  }
-
-  /**
-   * Interface that defines a listener that can be notified when new Applications are created.
-   */
-  public interface ApplicationCreatedListener {
-    void onApplicationCreated(Application application);
-  }
-
-  // Renders the list items in the location dropdown menu
-  private class AppEngineRegionComboBoxRenderer extends JLabel implements
-      ListCellRenderer<Location> {
-
-    @Override
-    public Component getListCellRendererComponent(JList<? extends Location> list, Location value,
-        int index, boolean isSelected, boolean cellHasFocus) {
-
-      // TODO consider right-padding for tabular format
-      String displayText = getLocationId(value);
-
-      boolean isStandardSupported
-          = parseMetatadaBoolean(STANDARD_ENV_AVAILABLE_KEY, value.getMetadata());
-      boolean isFlexSupported
-          = parseMetatadaBoolean(FLEXIBLE_ENV_AVAILABLE_KEY, value.getMetadata());
-
-      if (isStandardSupported && isFlexSupported) {
-        displayText += " (supports standard and flexible)";
-      } else if (isStandardSupported) {
-        displayText += " (supports standard)";
-      } else if (isFlexSupported) {
-        displayText += " (supports flexible)";
-      }
-
-      setText(displayText);
-      return this;
-    }
-
-    private boolean parseMetatadaBoolean(String key, Map<String, Object> metadata) {
-      if (!metadata.containsKey(key)) {
-        return false;
-      }
-      Object val = metadata.get(key);
-      if (val == null || !(val instanceof Boolean)) {
-        return false;
-      }
-      return (Boolean) val;
-    }
   }
 
 }
